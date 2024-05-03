6549d6746aa645bb149698d41b14d800168b9f32
Bermar Villarazo Jr <villarazobermar@gmail.com>
2024-05-03 22:44:54 +0800

    Zod validation both server and client are now good.

--- app/api/applicant/apply-now/route.ts
+++ app/api/applicant/apply-now/route.ts
@@ -1,7 +1,9 @@
 import { db } from "@/lib/db";
 import * as schema from "@/lib/schema";
 import { eq } from "drizzle-orm";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
+import validator from "validator";
+import { z } from "zod";
 
 export async function GET() {
     try {
@@ -23,39 +25,43 @@ export async function GET() {
     }
 }
 
-export async function POST(request: Request) {
+const applicantSchema = z.object({
+    first_Name: z
+        .string()
+        .min(2, { message: "First Name must have 2 or more characters" })
+        .max(75, { message: "First Name must have 75 or less characters" }),
+    last_Name: z
+        .string()
+        .min(2, { message: "Last Name must have 2 or more characters" })
+        .max(75, { message: "Last Name must have 75 or less characters" }),
+    email: z.string().email({ message: "Invalid email address" }),
+    contactNumber: z.string().refine((value) => validator.isMobilePhone(value, "en-PH"), {
+        message: "Invalid mobile phone number",
+    }),
+    communication: z.enum(["Email", "PhoneNumber"]),
+    position: z.enum(["teachingStaff", "non-teachingStaff"]),
+    resume: z.string(),
+});
+
+type applicantSchemaProps = z.infer<typeof applicantSchema>;
+
+export async function POST(request: NextRequest) {
     try {
-        const {
-            first_Name,
-            last_Name,
-            email,
-            contactNumber,
-            communication,
-            position,
-            resume,
-        }: schema.applicants = await request.json();
-
-        if (
-            !first_Name ||
-            !last_Name ||
-            !email ||
-            !contactNumber ||
-            !communication ||
-            !position ||
-            !resume
-        ) {
-            return NextResponse.json(
-                { message: "Please input all fields", status: 400 },
-                { status: 400 }
-            );
+        const data: applicantSchemaProps = await request.json();
+        // Client validation
+        const validationResult = applicantSchema.safeParse(data);
+        // Client validation
+        if (!validationResult.success) {
+            console.log(validationResult.error.issues);
+            return NextResponse.json(validationResult.error.issues, { status: 409 });
         }
-
+        // Database email validation if there is existing email
         const existingApplicant = await db
             .select()
             .from(schema.applicant)
-            .where(eq(schema.applicant.email, email));
-
-        if (existingApplicant) {
+            .where(eq(schema.applicant.email, data.email));
+        // Database email validation if there is existing email
+        if (existingApplicant.length > 0) {
             return NextResponse.json(
                 {
                     message: "Email already exists. Please use a different email.",
@@ -64,27 +70,13 @@ export async function POST(request: Request) {
                 { status: 409 }
             );
         }
-
+        // Applicant data insertion if no existing email, and client validation is successful
         await db.insert(schema.applicant).values({
-            first_Name,
-            last_Name,
-            email,
-            contactNumber,
-            resume,
-            communication,
-            position,
+            ...data,
+            contactNumber: parseInt(data.contactNumber),
         });
 
-        return NextResponse.json({
-            first_Name,
-            last_Name,
-            email,
-            contactNumber,
-            communication,
-            position,
-            resume,
-            status: 200,
-        });
+        return NextResponse.json({ data, status: 200 }, { status: 200 });
     } catch (error) {
         return NextResponse.json(
             { message: "Internal Server Error", status: 500 },
