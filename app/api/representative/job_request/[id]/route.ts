144ba7d3396e7cb746c8d77ff2925029e5c3e795
wanderpe <98376683+wanderpe@users.noreply.github.com>
2024-05-09 16:36:10 +0800

    Jobrequest Updates endpoint

--- app/api/representative/job_request/[id]/route.ts
+++ app/api/representative/job_request/[id]/route.ts
@@ -1,18 +1,49 @@
-import { ParamsProps, jobRequestSchema, jobRequestSchemaProps,  } from "@/app/types/type";
+import { ParamsProps, jobRequestSchema, jobRequestSchemaProps, } from "@/app/types/type";
 import { db } from "@/lib/db";
-import { NextRequest, NextResponse } from "next/server";
 import * as schema from "@/lib/schema";
 import { eq } from "drizzle-orm";
+import { NextRequest, NextResponse } from "next/server";
+
+
+export async function GET(response: NextResponse, request: NextRequest, {params} : ParamsProps) {
+    try {
+
+        const name = params.id
+        const existDepartment = await db.select().from(schema.department).where(eq(schema.department.department_code, name ))
+
+        
+        const result = await db.query.jobRequest.findFirst({
+            with: {
+                department: true,
+            },
+        });
+        return NextResponse.json({ result: result?.department });
+    } catch (error) {
+        console.log(error);
+        return NextResponse.json(
+            { message: "Internal Server Error", status: 500 },
+            { status: 500 }
+        );
+    }
+}
+
+
+
+
 
 
 
 export async function POST(request: Request, {params} : ParamsProps) {
     try {
-        const name = params.id
+        const id = params.id;
         const data: jobRequestSchemaProps = await request.json();
         const validationResult = jobRequestSchema.safeParse(data);
         let deptId;
         let officeId;
+        let deptName;
+        let officeName;
+
+    
 
         if(!validationResult.success){
             return NextResponse.json(validationResult.error.issues, { status: 409 });
@@ -34,32 +65,33 @@ export async function POST(request: Request, {params} : ParamsProps) {
             );
         }
 
-        const existDepartment = await db.select().from(schema.department).where(eq(schema.department.department_code, name ))
+        const existDepartment = await db.select().from(schema.department).where(eq(schema.department.department_name, id ))
+
         
         if(existDepartment.length > 0) {
-            existDepartment.forEach(({ department_id }) => {
+            existDepartment.forEach(({ department_id, department_name }) => {
             deptId = department_id;
+            deptName = department_name
             });
 
         }else{
             const existOffice = await db
                 .select()
                 .from(schema.office)
-                .where(eq(schema.office.office_name, name));
-            existOffice.forEach(({ office_id }) => {
+                .where(eq(schema.office.office_name, id));
+            existOffice.forEach(({ office_id, office_name }) => {
                 officeId = office_id;
+                officeName = office_name;
             });
         }
-
-        
-
         await db.insert(schema.jobRequest).values({
             ...data,
             departmentId: deptId,
             officeId: officeId,
+            officeName: officeName,
+            departmentName: deptName
         })
 
-       
         return NextResponse.json({ data }, { status: 200 });
 
     } catch (error) {
